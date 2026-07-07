d92f9c14ad6a92bd54d03acb55d31da016c0fd30
Mandrado Santos <mandrado@gmail.com>
2026-07-06 21:50:07 -0300

    Configuraçào do ESLint e Prettier. Adotano o modelo nvidia/nemotron-nano-9b-v2

--- modulo02-integracao-apis-llms/04-song-highlights/src/config.ts
+++ modulo02-integracao-apis-llms/04-song-highlights/src/config.ts
@@ -18,17 +18,18 @@ export type ModelConfig = {
   };
 };
 
-console.assert(process.env.OPENROUTER_API_KEY, 'OPENROUTER_API_KEY is not set in environment variables');
+console.assert(
+  process.env.OPENROUTER_API_KEY,
+  'OPENROUTER_API_KEY is not set in environment variables',
+);
 
 export const config: ModelConfig = {
   apiKey: process.env.OPENROUTER_API_KEY!,
   httpReferer: '',
   xTitle: 'IA Devs - Prompt Chaining Article Generator',
   models: [
-    // 'qwen/qwen3-coder-next',
     // https://openrouter.ai/models?fmt=cards&max_price=0&order=throughput-high-to-low&supported_parameters=structured_outputs%2Cresponse_format
-    'upstage/solar-pro-3:free',
-    // 'gpt-oss-120b:free',
+    'nvidia/nemotron-nano-9b-v2:free',
   ],
   provider: {
     sort: {
@@ -38,6 +39,7 @@ export const config: ModelConfig = {
   },
   temperature: 0.7,
   memory: {
-    dbUri: 'postgresql://postgres:mysecretpassword@localhost:5432/song_recommender',
-  }
+    dbUri:
+      'postgresql://postgres:mysecretpassword@localhost:5432/song_recommender',
+  },
 };
